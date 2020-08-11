b56de777304b2d71e7c0110f06126593ad47a609
Seaziicks <esteban.baichoo@gmail.com>
2020-08-11 19:14:58 +0200

    Adding first component and bootstrap import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LootTableComponent } from './loot-table/loot-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LootTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/loot-table/loot-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loot-table',
+  templateUrl: './loot-table.component.html',
+  styleUrls: ['./loot-table.component.scss']
+})
+export class LootTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
