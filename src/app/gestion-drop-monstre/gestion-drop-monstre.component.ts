52ac579b5a99b2a7e9b686f5db611b1ba06b173d
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-08-16 02:14:13 +0200

    Well, I don't know :o

---
+++ src/app/gestion-drop-monstre/gestion-drop-monstre.component.ts
@@ -0,0 +1,191 @@
+import {Component, IterableDiffers, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FamilleMonstreService} from '../services/famille-monstre.service';
+import {MonstreLootChanceService} from '../services/monstre-loot-chance.service';
+import {SpecialResponse} from '../loot-table/loot-table.component';
+
+
+export const filterMissing = (opt: MonstreLootChance[], values: any[]): string[] => {
+  return values.map(a => a.libelle).filter(f => !opt.map(a => a.libelle).includes(f));
+};
+
+@Component({
+  selector: 'app-gestion-drop-monstre',
+  templateUrl: './gestion-drop-monstre.component.html',
+  styleUrls: ['./gestion-drop-monstre.component.scss']
+})
+export class GestionDropMonstreComponent implements OnInit {
+
+  monstresGroupes: MonstreGroupe[];
+  monstreCourrant: Monstre;
+  monstreSelectionneLootChance: MonstreLootChance[];
+  monstreSelectionneLootChanceOriginal: MonstreLootChance[];
+  lootPossible: string[] = [];
+  addedRow: boolean[] = [];
+
+  unUpdatableMonstreLootChance: MonstreLootChance[] = [];
+  monstreLootChanceToPOST: MonstreLootChance[] = [];
+  monstreLootChanceToPUT: MonstreLootChance[] = [];
+  unchangesLootChance: MonstreLootChance[] = [];
+
+  differ: any;
+
+  constructor(private http: HttpClient,
+              private familleMonstre: FamilleMonstreService,
+              private monstreLootChance: MonstreLootChanceService,
+              ) {
+  }
+
+  ngOnInit(): void {
+    this.chargerFamilles(this.http);
+    this.monstreLootChance.getLootPossibles(this.http).then(
+      (data: any) => {
+        const response: SpecialResponse = JSON.parse(data) as SpecialResponse;
+        this.lootPossible  = response.data as string[];
+      }
+    );
+  }
+
+  public chargerFamilles(http: HttpClient) {
+    this.monstresGroupes = this.familleMonstre.chargerFamillesAvecMonstres(http);
+    console.log(JSON.stringify(this.monstresGroupes));
+  }
+
+  public chargerMonstreLootChance(http: HttpClient, idMonstre: number) {
+    this.monstreSelectionneLootChance = [];
+    this.addedRow = [];
+    this.monstreLootChance.getMonstreLootChanceTest(this.http, this.monstreCourrant.idMonstre).then(
+      (data: any) => {
+        const response: SpecialResponse = JSON.parse(data) as SpecialResponse;
+        this.monstreSelectionneLootChance  = response.data as MonstreLootChance[];
+        this.addedRow = this.remplissageLootManquant(this.monstreSelectionneLootChance , this.lootPossible);
+        this.monstreSelectionneLootChanceOriginal = this.monstreSelectionneLootChance .map(x => Object.assign({}, x));
+      });
+  }
+
+  public remplissageLootManquant(monstreSelectionneLootChance: MonstreLootChance[], lootPossible: string[]): boolean[] {
+    const addedRow: boolean[] = [];
+    for (const value of lootPossible) {
+      addedRow.push(false);
+    }
+    for (let i = 0; i < lootPossible.length; i++) {
+      if (!monstreSelectionneLootChance.map(a => a.libelle).includes(lootPossible[i])) {
+        monstreSelectionneLootChance.splice(i, 0,
+          {
+            idLoot: (i + 1), libelle: lootPossible[i], minRoll: null, maxRoll: null, niveauMonstre: null, multiplier: null,
+            dicePower: null, poids: null
+          } as unknown as MonstreLootChance);
+        addedRow[i] = true;
+      }
+    }
+    return addedRow;
+  }
+
+  selectionMonstre($event: Monstre) {
+    if (!this.monstreCourrant || this.monstreCourrant.idMonstre !== $event.idMonstre) {
+      this.monstreCourrant = $event;
+      this.chargerMonstreLootChance(this.http, $event.idMonstre);
+    }
+  }
+
+  public missingAtLeastOneValue(index: number) {
+    for (const key in this.monstreSelectionneLootChance[index]) {
+      if (this.monstreSelectionneLootChance[index][key] == null && key !== 'niveauMonstre' && key !== 'poids') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public thereIsAtLeastOneValue(index: number) {
+    for (const key in this.monstreSelectionneLootChance[index]) {
+      if (this.monstreSelectionneLootChance[index][key] != null && key !== 'niveauMonstre' && key !== 'poids' && key !== 'idLoot') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public originalLootChanceWasEmpty(index: number) {
+    for (const key in this.monstreSelectionneLootChanceOriginal[index]) {
+      if (this.monstreSelectionneLootChanceOriginal[index][key] != null && key !== 'niveauMonstre' && key !== 'poids'
+        && key !== 'idLoot' && key !== 'libelle') {
+        return false;
+      }
+    }
+    console.log('originalLootChanceWasEmpty => true');
+    return true;
+  }
+
+  public missingMyValue(index: number, key: string) {
+    return this.monstreSelectionneLootChance[index][key] == null;
+  }
+
+  public lootChanceIsCorrect(index: number) {
+    for (const key in this.monstreSelectionneLootChance[index]) {
+      if (this.monstreSelectionneLootChance[index][key] == null && key !== 'niveauMonstre' && key !== 'poids') {
+        console.log('La clef ' + key + 'est null');
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public lootChanceIsDifferentFromOriginalOne(index: number) {
+    return !(JSON.stringify(this.monstreSelectionneLootChanceOriginal[index]) === JSON.stringify(this.monstreSelectionneLootChance[index]));
+  }
+
+  public updateOrCreateLootChance() {
+    for (let i = 0; i < this.lootPossible.length; i++) {
+      console.log(this.monstreSelectionneLootChance[i]);
+      if (this.thereIsAtLeastOneValue(i) && this.missingAtLeastOneValue(i)) {
+        console.log('Ajout dans unUpdatable');
+        this.unUpdatableMonstreLootChance.push(this.monstreSelectionneLootChance[i]);
+      } else if (this.originalLootChanceWasEmpty(i) && this.lootChanceIsCorrect(i)) {
+        console.log('Ajout dans POST');
+        this.monstreLootChanceToPOST.push(this.monstreSelectionneLootChance[i]);
+      } else if (this.lootChanceIsCorrect(i) && this.lootChanceIsDifferentFromOriginalOne(i)) {
+        console.log('Ajout dans PUT');
+        this.monstreLootChanceToPUT.push(this.monstreSelectionneLootChance[i]);
+      } else {
+        console.log('Ajout dans unChanged');
+        this.unchangesLootChance.push(this.monstreSelectionneLootChance[i]);
+      }
+      console.log((this.unUpdatableMonstreLootChance.length + this.monstreLootChanceToPOST.length
+        + this.monstreLootChanceToPUT.length + this.unchangesLootChance.length));
+    }
+    if (this.monstreLootChanceToPOST.length > 0) {
+      this.monstreLootChance.envoyerLootChances(this.http, 'POST', this.monstreCourrant.idMonstre, this.monstreLootChanceToPOST).then(
+        (data: any) => {
+          console.log(data);
+          this.monstreLootChanceToPOST = [];
+          this.monstreLootChanceToPUT = [];
+          this.unUpdatableMonstreLootChance = [];
+          this.unchangesLootChance = [];
+        });
+    }
+    if (this.monstreLootChanceToPUT.length > 0) {
+      this.monstreLootChance.envoyerLootChances(this.http, 'PUT', this.monstreCourrant.idMonstre, this.monstreLootChanceToPUT).then(
+        (data: any) => {
+          console.log(data);
+          this.monstreLootChanceToPOST = [];
+          this.monstreLootChanceToPUT = [];
+          this.unUpdatableMonstreLootChance = [];
+          this.unchangesLootChance = [];
+        });
+    }
+  }
+
+  public uncompleteForm(): boolean {
+    if (this.monstreSelectionneLootChance) {
+      for (let i = 0; i < this.monstreSelectionneLootChance.length; i++) {
+        if (this.missingAtLeastOneValue(i)) {
+          return true;
+        }
+      }
+      return false;
+    }
+    return false;
+  }
+
+}
--- src/app/gestion-monstre/gestion-monstre.component.ts
+++ src/app/gestion-monstre/gestion-monstre.component.ts
@@ -1,13 +1,10 @@
-import {Component, IterableDiffers, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FamilleMonstreService} from '../services/famille-monstre.service';
-import {MonstreLootChanceService} from '../services/monstre-loot-chance.service';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 import {SpecialResponse} from '../loot-table/loot-table.component';
-
-
-export const filterMissing = (opt: MonstreLootChance[], values: any[]): string[] => {
-  return values.map(a => a.libelle).filter(f => !opt.map(a => a.libelle).includes(f));
-};
 
 @Component({
   selector: 'app-gestion-monstre',
@@ -16,178 +13,52 @@ export const filterMissing = (opt: MonstreLootChance[], values: any[]): string[]
 })
 export class GestionMonstreComponent implements OnInit {
 
+
+  myControl = new FormControl();
+  familles: Famille[];
+  familleCourante: Famille;
+  filteredFamilles: Observable<Famille[]>;
+
   monstresGroupes: MonstreGroupe[];
   monstreCourrant: Monstre;
-  monstreSelectionneLootChance: MonstreLootChance[];
-  monstreSelectionneLootChanceOriginal: MonstreLootChance[];
-  lootPossible: string[] = [];
-  addedRow: boolean[] = [];
-
-  unUpdatableMonstreLootChance: MonstreLootChance[] = [];
-  monstreLootChanceToPOST: MonstreLootChance[] = [];
-  monstreLootChanceToPUT: MonstreLootChance[] = [];
-  unchangesLootChance: MonstreLootChance[] = [];
-
-  differ: any;
 
   constructor(private http: HttpClient,
               private familleMonstre: FamilleMonstreService,
-              private monstreLootChance: MonstreLootChanceService,
-              differs: IterableDiffers,
   ) {
-    this.differ = differs.find([]).create(null);
   }
 
   ngOnInit(): void {
-    this.chargerFamilles(this.http);
-    this.monstreLootChance.getLootPossibles(this.http).then(
+    // this.familles = this.familleMonstre.chargerAllFamilles(this.http);
+    this.familleMonstre.getAllFamilles(this.http).then(
       (data: any) => {
         const response: SpecialResponse = JSON.parse(data) as SpecialResponse;
-        this.lootPossible  = response.data as string[];
+        this.familles  = response.data as Famille[];
+        console.log(this.familles);
+        this.filteredFamilles = this.myControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this.filter(value))
+        );
       }
     );
+    this.monstresGroupes = this.familleMonstre.chargerFamillesAvecMonstres(this.http);
   }
 
-  public chargerFamilles(http: HttpClient) {
-    this.monstresGroupes = this.familleMonstre.chargerFamilles(http);
-    console.log(JSON.stringify(this.monstresGroupes));
-  }
-
-  public chargerMonstreLootChance(http: HttpClient, idMonstre: number) {
-    this.monstreSelectionneLootChance = [];
-    this.addedRow = [];
-    this.monstreLootChance.getMonstreLootChanceTest(this.http, this.monstreCourrant.idMonstre).then(
-      (data: any) => {
-        const response: SpecialResponse = JSON.parse(data) as SpecialResponse;
-        this.monstreSelectionneLootChance  = response.data as MonstreLootChance[];
-        this.addedRow = this.remplissageLootManquant(this.monstreSelectionneLootChance , this.lootPossible);
-        this.monstreSelectionneLootChanceOriginal = this.monstreSelectionneLootChance .map(x => Object.assign({}, x));
-      });
-  }
-
-  public remplissageLootManquant(monstreSelectionneLootChance: MonstreLootChance[], lootPossible: string[]): boolean[] {
-    const addedRow: boolean[] = [];
-    for (const value of lootPossible) {
-      addedRow.push(false);
+  private filter(value: string): Famille[] {
+    if (value) {
+      const filterValue = value.toLowerCase();
+      return this.familles.filter(option => option.libelle.toLowerCase().indexOf(filterValue) === 0);
     }
-    for (let i = 0; i < lootPossible.length; i++) {
-      if (!monstreSelectionneLootChance.map(a => a.libelle).includes(lootPossible[i])) {
-        monstreSelectionneLootChance.splice(i, 0,
-          {
-            idLoot: (i + 1), libelle: lootPossible[i], minRoll: null, maxRoll: null, niveauMonstre: null, multiplier: null,
-            dicePower: null, poids: null
-          } as unknown as MonstreLootChance);
-        addedRow[i] = true;
-      }
-    }
-    return addedRow;
+    return this.familles;
   }
+
 
   selectionMonstre($event: Monstre) {
     if (!this.monstreCourrant || this.monstreCourrant.idMonstre !== $event.idMonstre) {
       this.monstreCourrant = $event;
-      this.chargerMonstreLootChance(this.http, $event.idMonstre);
     }
   }
 
-  public missingAtLeastOneValue(index: number) {
-    for (const key in this.monstreSelectionneLootChance[index]) {
-      if (this.monstreSelectionneLootChance[index][key] == null && key !== 'niveauMonstre' && key !== 'poids') {
-        return true;
-      }
-    }
-    return false;
+  selectionFamille(event) {
+    this.familleCourante = this.familles.filter(option => option.libelle.toLowerCase().indexOf(event.option.value.toLowerCase()) === 0)[0];
   }
-
-  public thereIsAtLeastOneValue(index: number) {
-    for (const key in this.monstreSelectionneLootChance[index]) {
-      if (this.monstreSelectionneLootChance[index][key] != null && key !== 'niveauMonstre' && key !== 'poids' && key !== 'idLoot') {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public originalLootChanceWasEmpty(index: number) {
-    for (const key in this.monstreSelectionneLootChanceOriginal[index]) {
-      if (this.monstreSelectionneLootChanceOriginal[index][key] != null && key !== 'niveauMonstre' && key !== 'poids'
-        && key !== 'idLoot' && key !== 'libelle') {
-        return false;
-      }
-    }
-    console.log('originalLootChanceWasEmpty => true');
-    return true;
-  }
-
-  public missingMyValue(index: number, key: string) {
-    return this.monstreSelectionneLootChance[index][key] == null;
-  }
-
-  public lootChanceIsCorrect(index: number) {
-    for (const key in this.monstreSelectionneLootChance[index]) {
-      if (this.monstreSelectionneLootChance[index][key] == null && key !== 'niveauMonstre' && key !== 'poids') {
-        console.log('La clef ' + key + 'est null');
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public lootChanceIsDifferentFromOriginalOne(index: number) {
-    return !(JSON.stringify(this.monstreSelectionneLootChanceOriginal[index]) === JSON.stringify(this.monstreSelectionneLootChance[index]));
-  }
-
-  public updateOrCreateLootChance() {
-    for (let i = 0; i < this.lootPossible.length; i++) {
-      console.log(this.monstreSelectionneLootChance[i]);
-      if (this.thereIsAtLeastOneValue(i) && this.missingAtLeastOneValue(i)) {
-        console.log('Ajout dans unUpdatable');
-        this.unUpdatableMonstreLootChance.push(this.monstreSelectionneLootChance[i]);
-      } else if (this.originalLootChanceWasEmpty(i) && this.lootChanceIsCorrect(i)) {
-        console.log('Ajout dans POST');
-        this.monstreLootChanceToPOST.push(this.monstreSelectionneLootChance[i]);
-      } else if (this.lootChanceIsCorrect(i) && this.lootChanceIsDifferentFromOriginalOne(i)) {
-        console.log('Ajout dans PUT');
-        this.monstreLootChanceToPUT.push(this.monstreSelectionneLootChance[i]);
-      } else {
-        console.log('Ajout dans unChanged');
-        this.unchangesLootChance.push(this.monstreSelectionneLootChance[i]);
-      }
-      console.log((this.unUpdatableMonstreLootChance.length + this.monstreLootChanceToPOST.length
-        + this.monstreLootChanceToPUT.length + this.unchangesLootChance.length));
-    }
-    if (this.monstreLootChanceToPOST.length > 0) {
-      this.monstreLootChance.envoyerLootChances(this.http, 'POST', this.monstreCourrant.idMonstre, this.monstreLootChanceToPOST).then(
-        (data: any) => {
-          console.log(data);
-          this.monstreLootChanceToPOST = [];
-          this.monstreLootChanceToPUT = [];
-          this.unUpdatableMonstreLootChance = [];
-          this.unchangesLootChance = [];
-        });
-    }
-    if (this.monstreLootChanceToPUT.length > 0) {
-      this.monstreLootChance.envoyerLootChances(this.http, 'PUT', this.monstreCourrant.idMonstre, this.monstreLootChanceToPUT).then(
-        (data: any) => {
-          console.log(data);
-          this.monstreLootChanceToPOST = [];
-          this.monstreLootChanceToPUT = [];
-          this.unUpdatableMonstreLootChance = [];
-          this.unchangesLootChance = [];
-        });
-    }
-  }
-
-  public uncompleteForm(): boolean {
-    if (this.monstreSelectionneLootChance) {
-      for (let i = 0; i < this.monstreSelectionneLootChance.length; i++) {
-        if (this.missingAtLeastOneValue(i)) {
-          return true;
-        }
-      }
-      return false;
-    }
-    return false;
-  }
-
 }
