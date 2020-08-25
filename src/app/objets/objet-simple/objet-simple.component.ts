1be0457fbd9e582cd9e26b7c326d871df556b44f
Esteban Baichoo <esteban.baichoo@gmail.com>
2020-08-25 10:30:20 +0200

    Adding Malediction part. Committing because I saw an error in data. Malediction special Object miss <a> tag when getting text.

--- src/app/objets/objet-simple/objet-simple.component.ts
+++ src/app/objets/objet-simple/objet-simple.component.ts
@@ -29,16 +29,17 @@ export class ObjetSimpleComponent implements OnInit {
     maudit = true;
     malediction: MagicalProperty;
     deMalediction: number;
+    deMalediction2: number;
+    // ---------------------------------------
     conditionsExterieur: MagicalProperty;
-    conditions: TablesChances;
-    deCondition: number;
-    condition: string;
+    // ---------------------------------------
+    conditionsDeFonctionnement: TablesChances;
+    conditionDeFonctionnement: string;
+    // ----------------------------------------
     inconveniants: TablesChances;
     inconveniant: string;
-    deInconveniant: number;
-
+    // ----------------------------------------
     allObjetsMaudits: SortedMagicalProperty;
-    deMauditSpecial: number;
     dePuissanceMalediction: number;
 
     constructor(private  http: HttpClient, private jsonService: JSonLoadService) {
@@ -51,40 +52,40 @@ export class ObjetSimpleComponent implements OnInit {
     ngOnInit(): void {
         this.jsonService.getJSON(this.parametres[3], this.parametres[4]).then(
             (effetsObjet: any) => {
-                console.log(effetsObjet);
+                // console.log(effetsObjet);
                 this.allObjets = JSON.parse(effetsObjet) as SortedMagicalProperty;
-                console.log(JSON.parse(effetsObjet).strongAnfPowerful[9]);
-                console.log(this.allObjets.strongAnfPowerful[9]);
+                // console.log(JSON.parse(effetsObjet).strongAnfPowerful[9]);
+                // console.log(this.allObjets.strongAnfPowerful[9]);
             }
         );
         if (this.maudit) {
             this.jsonService.getJSON('magique', 'inconvenientsMalediction').then(
                 (data: any) => {
-                    console.log(data);
+                    // console.log(data);
                     this.inconveniants = JSON.parse(data) as TablesChances;
-                    console.log(this.inconveniants);
+                    // console.log(this.inconveniants);
                 }
             );
             this.jsonService.getJSON('magique', 'conditionsMalediction').then(
                 (data: any) => {
-                    console.log(data);
-                    this.conditions = JSON.parse(data) as TablesChances;
-                    console.log(this.conditions);
+                    // console.log(data);
+                    this.conditionsDeFonctionnement = JSON.parse(data) as TablesChances;
+                    // console.log(this.conditions);
                 }
             );
             this.jsonService.getJSON('magique', 'conditionExterieurMaledition').then(
                 (data: any) => {
-                    console.log(data);
+                    // console.log(data);
                     this.conditionsExterieur = JSON.parse(data) as MagicalProperty;
-                    console.log(this.conditionsExterieur);
+                    // console.log(this.conditionsExterieur);
                 }
             );
             this.jsonService.getJSON('magique', 'objetsMauditsSpeciaux').then(
                 (mauditsSpeciaux: any) => {
-                    console.log(mauditsSpeciaux);
+                    // console.log(mauditsSpeciaux);
                     this.allObjetsMaudits = JSON.parse(mauditsSpeciaux) as SortedMagicalProperty;
-                    console.log(JSON.parse(mauditsSpeciaux).strongAnfPowerful[9]);
-                    console.log(this.allObjetsMaudits.strongAnfPowerful[9]);
+                    // console.log(JSON.parse(mauditsSpeciaux).strongAnfPowerful[9]);
+                    // console.log(this.allObjetsMaudits.strongAnfPowerful[9]);
                 }
             );
         }
@@ -155,9 +156,15 @@ export class ObjetSimpleComponent implements OnInit {
         return index;
     }
 
+    resetMalediction() {
+        this.deMalediction2 = null;
+        this.malediction = null;
+    }
+
     getMalediction(): MagicalProperty {
+        this.malediction = null;
         let magicalProperty: MagicalProperty;
-        if (this.dePuissance <= 11) { // Imaginaire
+        if (this.deMalediction <= 11) { // Imaginaire
             magicalProperty = {
                 description: ['Le possesseur pense que l’objet est ce qu’il semble être, alors qu’il n’a d’autre pouvoir que d’abuser son utilisateur. Ce dernier est persuadé que l’objet fonctionne normalement et seul le sort délivrance des malédictions peut le convaincre du contraire.'],
                 infos: null,
@@ -165,7 +172,7 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Effet imaginaire.',
                 ul: null,
             } as MagicalProperty;
-        } else if (this.dePuissance > 13 && this.dePuissance <= 23) { // Inversé
+        } else if (this.deMalediction > 11 && this.deMalediction <= 23) { // Inversé
             magicalProperty = {
                 description: ['Soit les objets maudits de ce genre ont un effet inverse de celui qui était prévu, soit ils affectent leur possesseur au lieu de ses adversaires. Certains de ces objets peuvent toutefois présenter un intérêt indéniable, même si leur fonction est totalement différente de ce qu’elle devrait être.\n' +
                 'Parmi les objets maudits à effet opposé, on trouve les armes qui s’accompagnent d’un malus aux jets d’attaque et de dégâts. De même que les personnages ne connaissent pas immédiatement la valeur exacte du bonus d’altération conféré par leurs armes utiles, il n’y a aucune raison qu’ils se rendent compte immédiatement que leur nouvelle arme est maudite. Une fois qu’ils le comprennent, ils peuvent s’en séparer librement, à moins que l’arme ne s’accompagne d’un enchantement incitant son possesseur à vouloir la garder et à l’utiliser. Dans ce cas, le sort délivrance des malédictions est nécessaire pour s’en débarrasser.'],
@@ -174,7 +181,7 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Effet (ou cible) inversé.',
                 ul: null,
             } as MagicalProperty;
-        } else if (this.dePuissance > 26 && this.dePuissance <= 34) { // Fiabilité douteuse
+        } else if (this.deMalediction > 23 && this.deMalediction <= 34) { // Fiabilité douteuse
             magicalProperty = {
                 description: ['Chaque fois que l’objet est activé, il y a 5% de chances (01–05 sur 1d100) qu’il ne fonctionne pas. Ce risque de dysfonctionnement peut varier entre 1% et 10%, selon la campagne et le type d’objet.'],
                 infos: null,
@@ -182,7 +189,7 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Fiabilité douteuse.',
                 ul: null,
             } as MagicalProperty;
-        } else if (this.dePuissance > 39 && this.dePuissance <= 46) { // Condition extèrieure
+        } else if (this.deMalediction > 34 && this.deMalediction <= 46) { // Condition extèrieure
             magicalProperty = {
                 description: [''],
                 infos: null,
@@ -190,11 +197,11 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Dépendant d’une condition extérieure',
                 ul: null,
             } as MagicalProperty;
-            if (this.deMalediction) {
-                this.condition = this.getFromChance(this.conditions, this.deMalediction);
+            if (this.deMalediction2) {
+                this.conditionDeFonctionnement = this.getFromChance(this.conditionsDeFonctionnement, this.deMalediction2);
             }
-            magicalProperty.description[0] = this.condition;
-        } else if (this.dePuissance > 52 && this.dePuissance <= 57) { // Incontrolable
+            magicalProperty.description[0] = this.conditionDeFonctionnement;
+        } else if (this.deMalediction > 46 && this.deMalediction <= 57) { // Incontrolable
             magicalProperty = {
                 description: ['Un objet incontrôlable se met à fonctionner n’importe quand, sans que son propriétaire le décide. ' +
                 'Jetez 1d100 chaque jour. ' +
@@ -205,9 +212,9 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Incontrôlable.',
                 ul: null,
             } as MagicalProperty;
-        } else if (this.dePuissance > 65 && this.dePuissance <= 69) { // Conditions de fonctionnement
+        } else if (this.deMalediction > 57 && this.deMalediction <= 69) { // Conditions de fonctionnement
             magicalProperty = this.conditionsExterieur;
-        } else if (this.dePuissance > 78 && this.dePuissance <= 80) { // Inconveniant
+        } else if (this.deMalediction > 69 && this.deMalediction <= 80) { // Inconveniant
             magicalProperty = {
                 description: [''],
                 infos: null,
@@ -215,24 +222,41 @@ export class ObjetSimpleComponent implements OnInit {
                 title: 'Inconvéniant',
                 ul: null,
             } as MagicalProperty;
-            if (this.deMalediction) {
-                this.inconveniant = this.getFromChance(this.inconveniants, this.deMalediction);
+            if (this.deMalediction2) {
+                this.inconveniant = this.getFromChance(this.inconveniants, this.deMalediction2);
             }
             magicalProperty.description[0] = this.inconveniant;
-        } else if (this.dePuissance > 80 && this.dePuissance <= 92) { // Effet inventé
-                magicalProperty = {
-                    description: ['Choisissez un effet préjudiciable, en vous inspirant éventuellement des objets maudits spéciaux, détaillés ci-dessous. ' +
-                    'L’objet peut commencer à fonctionner normalement, mais à un moment, ses propriétés changent radicalement.'],
-                    infos: null,
-                    table: null,
-                    title: 'Effet radicalement différent.',
-                    ul: null,
-                } as MagicalProperty;
-        } else if (this.dePuissance > 92) { // Objet maudit spécial
-            if (this.deMauditSpecial) {
-                magicalProperty = JSON.parse(JSON.stringify(this.getObjetsMaudits()[this.deMauditSpecial - 1])) as MagicalProperty;
+        } else if (this.deMalediction > 80 && this.deMalediction <= 92) { // Effet inventé
+            magicalProperty = {
+                description: ['Choisissez un effet préjudiciable, en vous inspirant éventuellement des objets maudits spéciaux, détaillés ci-dessous. ' +
+                'L’objet peut commencer à fonctionner normalement, mais à un moment, ses propriétés changent radicalement.'],
+                infos: null,
+                table: null,
+                title: 'Effet radicalement différent.',
+                ul: null,
+            } as MagicalProperty;
+        } else if (this.deMalediction > 92) { // Objet maudit spécial
+            if (this.deMalediction2 && this.dePuissanceMalediction) {
+                magicalProperty = JSON.parse(JSON.stringify(this.getObjetsMaudits()[this.deMalediction2 - 1])) as MagicalProperty;
             }
         }
+        this.malediction = magicalProperty;
         return magicalProperty;
+    }
+
+    isInconveniant(): boolean {
+        return this.deMalediction > 69 && this.deMalediction <= 80;
+    }
+
+    isConditionDeFonctionnement(): boolean {
+        return this.deMalediction > 57 && this.deMalediction <= 69;
+    }
+
+    isConditionExterieure(): boolean {
+        return this.deMalediction > 34 && this.deMalediction <= 46;
+    }
+
+    isObjetMauditSpecial(): boolean {
+        return this.deMalediction > 92;
     }
 }
